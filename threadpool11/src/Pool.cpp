43ea789990127ceae9e388b0d3e6abf550396098
fasdfasdas <fasdfasdas@gmail.com>
2013-05-12 02:50:24 +0300

    Altered Pool.cpp and Worker.cpp to point the new include directories.

--- threadpool11/src/Pool.cpp
+++ threadpool11/src/Pool.cpp
@@ -27,8 +27,7 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 */
 
-#include "Pool/Pool.h"
-#include "Worker/Worker.h"
+#include "threadpool11/threadpool11.h"
 
 namespace threadpool11
 {
--- threadpool11/src/Worker.cpp
+++ threadpool11/src/Worker.cpp
@@ -27,8 +27,7 @@ of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 */
 
-#include "Worker/Worker.h"
-#include "Pool/Pool.h"
+#include "threadpool11/threadpool11.h"
 
 namespace threadpool11
 {
