28438f77f97f9c158b069e1f83b4fb74d95c835b
Tolga HOŞGÖR <fasdfasdas@gmail.com>
2014-01-14 00:04:44 +0200

    lockfree tests

--- threadpool11_demo/src/tests.cpp
+++ threadpool11_demo/src/tests.cpp
@@ -4,9 +4,9 @@
 
 #include "threadpool11/threadpool11.h"
 
-#define TEST_1
+//#define TEST_1
 #define TEST_2
-#define TEST_0
+//#define TEST_0
 
 int recursiveWork(threadpool11::Pool & pool, int depth ){
     if(depth<=0)
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
     try{
         int threads = 2;
         int recursion = 100;
-        int initial_jobs = 10000;
+        int initial_jobs = 100000;
         auto t0 = std::chrono::high_resolution_clock::now();
         #ifdef TEST_2
         threadpool11::Pool pool(threads);
