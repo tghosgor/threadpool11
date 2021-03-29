8998e5ca2fd2ea43bd5e74ef1deb3f0e906c3b3f
Tolga Hoşgör <fasdfasdas@gmail.com>
2021-03-29 10:58:37 +0300

    chore: change license to MIT

--- threadpool11/test/threadpool11_test.cpp
+++ threadpool11/test/threadpool11_test.cpp
@@ -1,22 +1,3 @@
-/**
- * threadpool11
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019 Tolga HOSGOR
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
 #include <threadpool11/pool.hpp>
 
 #include <gtest/gtest.h>
